d7fc3be7837e46d086d63592d34d1b905b55ea43
Kartik Angiras <angiraskartik@gmail.com>
2025-12-31 23:38:20 +0530

    tools

--- ui/src/components/TextCleanup.tsx
+++ ui/src/components/TextCleanup.tsx
@@ -7,3 +7,31 @@ interface TextCleanupProps {
   onOutput: (output: string) => void;
 }
 
+const TextCleanup: React.FC<TextCleanupProps> = ({ input, onOutput }) => {
+  const handleCleanup = (action: string) => {
+    const result = cleanupText(input, action);
+    onOutput(result);
+  };
+
+  const tools = [
+    {
+      action: 'removeExtraSpaces',
+      label: 'Remove Extra Spaces',
+      description: 'Collapse multiple spaces into single spaces'
+    },
+    {
+      action: 'removeLineBreaks',
+      label: 'Remove Line Breaks',
+      description: 'Convert text to single line'
+    },
+    {
+      action: 'removeAllSpaces',
+      label: 'Remove All Spaces',
+      description: 'Remove all whitespace characters'
+    },
+    {
+      action: 'trimLines',
+      label: 'Trim Each Line',
+      description: 'Remove leading/trailing spaces from each line'
+    }
+  ];
