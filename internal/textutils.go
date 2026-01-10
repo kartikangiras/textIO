aacabaa315f58c7a4037c2f56244fcab344cb0f0
Kartik Angiras <angiraskartik@gmail.com>
2026-01-10 19:16:24 +0530

    add textstats method

--- internal/textutils.go
+++ internal/textutils.go
@@ -1,1 +1,23 @@
 package internal
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+func getTextStats(input string) (int, int, int, int, error) {
+	characterCount := utf8.RuneCountInString(input)
+
+	words := strings.Fields(input)
+	wordCount := len(words)
+
+	lineCount := strings.Count(input, "\n") + 1
+
+	noSpaceCount := 0
+	for _, r := range input {
+		if r != ' ' && r != '\t' && r != '\n' {
+			noSpaceCount++
+		}
+	}
+	return characterCount, wordCount, lineCount, noSpaceCount, nil
+}
